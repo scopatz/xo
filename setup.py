dd1d2036e2a27ee04bedc181a57b45d199bc8d19
Anthony Scopatz <scopatz@gmail.com>
2018-04-19 16:06:13 -0400

    bumped version to 0.2.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ have_setuptools = False
 if sys.version_info[0] < 3:
     sys.exit("ERROR: xo requires Python 3.")
 
-VERSION = '0.2.1'
+VERSION = '0.2.2'
 
 setup_kwargs = {
     "version": VERSION,
