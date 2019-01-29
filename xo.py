f0fbefd3368dd3596d330218fe7f293f10624a10
Anthony Scopatz <scopatz@gmail.com>
2019-01-29 14:30:32 -0500

    bumped version to 0.3.2

--- xo.py
+++ xo.py
@@ -41,7 +41,7 @@ from pygments.token import Token
 from pygments.filter import Filter
 from pygments.styles import get_all_styles
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 RE_WORD = re.compile(r'\w+')
 RE_NOT_WORD = re.compile(r'\W+')
